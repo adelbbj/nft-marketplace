61552cd79559fc480947ae5a26f40019e0a49f69
adelbbj <adelbbj@gmail.com>
2023-02-04 14:52:23 +0330

    refactor folder structure

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,1 @@
-export { default as Navbar } from "./ui/navbar";
-export { default as BaseLayout } from "./layout/BaseLayout";
-export { default as NftList } from "./nft/list";
-export { default as NftItem } from "./nft/item";
-export { default as ActiveLink } from "./ui/Link";
+export * from "./ui";
--- components/ui/layout/BaseLayout.tsx
+++ components/ui/layout/BaseLayout.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
-import Navbar from "../ui/navbar";
+import Navbar from "../navbar";
 
 interface IBaseLayout {
   children: React.ReactNode;
